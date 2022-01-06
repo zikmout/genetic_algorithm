cd89edf879bb93b5ae2d3a1d4e9e69a9f695fbc5
Simon Sicard <sicsim64@gmail.com>
2022-01-06 12:55:33 +0100

    getting all solutions ok

--- Planning.js
+++ Planning.js
@@ -43,13 +43,51 @@ class Planning {
     this.amoNb = getAmoNb(grid);
   }
 
-  getSlotsForLength(ptr) {
-    let amoSolutions = {};
+  getSlotsForLength(amoNb) {
+    let S = {};
+    let ptr = this.getAmosList()[amoNb].head;
+    let planningStart = this.getAmosList()[amoNb].planningStart;
+    // { start: 1635751800000, amoNb: 0, length: 2, booked: false }
+    let name = undefined;
+    let start = undefined;
+    let counter = 1;
+    // start = ptr.data.start;
+
     while (ptr !== null) {
-      console.log(`ptr.data : ${JSON.stringify(ptr.data)}`);
+      // console.log(`ptr.data : ${JSON.stringify(ptr.data)}`);
+      if (
+        ptr.data.booked !== undefined &&
+        ptr.data.booked !== false &&
+        !ptr.data.booked.includes("@")
+      ) {
+        if (ptr.data.booked === ptr.next.data.booked) {
+          if (start === undefined) {
+            start = ptr.data.start;
+          }
+          // console.log(`1 ptr : ${ptr.data.booked}`);
+          counter++;
+        } else {
+          // console.log(`2 ptr : ${ptr.data.booked}`);
+          // console.log(`${ptr.data.booked} : ${counter}`);
+          let sol = {
+            start: planningStart + start,
+            amoNb: 0,
+            length: counter,
+            booked: false,
+          };
+          if (S[counter]) {
+            S[counter].push(sol);
+          } else {
+            S[counter] = [sol];
+          }
+          counter = 1;
+          start = ptr.data.start;
+        }
+      }
       ptr = ptr.next;
     }
-    return amoSolutions;
+
+    return S;
   }
 
   getSolutions() {
@@ -57,10 +95,26 @@ class Planning {
     // this.getSlotsForLength(this.getAmosList()[4].head);
     for (let amo = 0; amo < this.getAmosList().length; amo++) {
       console.log(`margin after : ${this.getAmosList()[amo].getMargin()}`);
-      let amoSolutions = this.getSlotsForLength(this.getAmosList()[amo].head);
-      console.log(`amoSolutions --> ${JSON.stringify(amoSolutions)}`);
-      return;
+      let amoSolutions = this.getSlotsForLength(amo);
+      console.log(`\n\namoSolutions --> ${JSON.stringify(amoSolutions)}`);
+      for (const [key, value] of Object.entries(amoSolutions)) {
+        if (S[key] === undefined) S[key] = [];
+        for (let v = 0; v < value.length; v++) {
+          if (S[key].find((_) => _.start === value[v].start) === undefined) {
+            S[key].push(value[v]);
+          }
+        }
+      }
+      // return;
     }
+
+    Object.keys(S).forEach((obj) => {
+      S[obj] = S[obj].sort(function (a, b) {
+        return a.start - b.start;
+      });
+    });
+
+    console.log(S);
   }
 
   getTotalPeople() {
